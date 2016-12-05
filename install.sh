af511dafd3e1e6784973fa51ec5fa2c5b886fb1c
mjuniper <mjuniper@gmail.com>
2016-12-05 13:15:54 -0700

    lean on mackup

--- install.sh
+++ install.sh
@@ -1,35 +1,12 @@
 #!/usr/bin/env bash
 
-# from: https://github.com/webpro/dotfiles
-
-# Get current dir (so run this script from anywhere)
-
-export DOTFILES_DIR EXTRA_DIR
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-EXTRA_DIR="$HOME/.extra"
-
-# Update dotfiles itself first
-
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
-
-# Bunch of symlinks
-
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-# ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
-# ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-
 # os x command line tools
-
 # xcode-select --install
 
 # Package managers & packages
 
-. "$DOTFILES_DIR/install/brew.sh"
-# . "$DOTFILES_DIR/install/bash.sh"
-. "$DOTFILES_DIR/install/npm.sh"
+./install/brew.sh
+./install/brew-cask.sh
+./install/npm.sh
 
-
-. "$DOTFILES_DIR/install/brew-cask.sh"
-# ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
+mackup restore
